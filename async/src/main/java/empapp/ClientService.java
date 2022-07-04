b6717a7070390efa5fecacd6c7b3884b2977a30a
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-04 15:45:45 +0200

    WebClient

--- async/src/main/java/empapp/ClientService.java
+++ async/src/main/java/empapp/ClientService.java
@@ -3,8 +3,14 @@ package empapp;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Service
 @Slf4j
@@ -24,9 +30,19 @@ public class ClientService {
             log.error("interrupted", ie);
         }
 
-        String status = "200";
-        publisher.publishEvent(new CallCompletedEvent(id, status));
+        // OpenFeign
+        // Spring WebClient - reaktív
+        // Spring Project Reactor
+        HttpClient client = HttpClient.create().responseTimeout(Duration.ofSeconds(5));
 
-        log.info("Get status end");
+        WebClient.builder().clientConnector(new ReactorClientHttpConnector(client)).build()
+                .get().uri("https://training360.com")
+                .exchangeToMono(clientResponse -> Mono.just(clientResponse.statusCode().toString()))
+                .subscribe(status -> publisher.publishEvent(new CallCompletedEvent(id, status)));
+
+//        String status = "200";
+//        publisher.publishEvent(new CallCompletedEvent(id, status));
+//
+//        log.info("Get status end");
     }
 }
