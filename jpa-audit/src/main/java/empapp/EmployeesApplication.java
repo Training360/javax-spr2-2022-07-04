f5baff06f8f0ab0a075e4a893fe620fd9459bbb8
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-04 10:33:44 +0200

    Init

--- jpa-audit/src/main/java/empapp/EmployeesApplication.java
+++ jpa-audit/src/main/java/empapp/EmployeesApplication.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class EmployeesApplication
 {
 
