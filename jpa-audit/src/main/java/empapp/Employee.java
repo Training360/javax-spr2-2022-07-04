1ffcea1dd207ec7bcecbd654b4548fc8d82e0574
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-04 11:04:08 +0200

    Spring Data JPS Audit

--- jpa-audit/src/main/java/empapp/Employee.java
+++ jpa-audit/src/main/java/empapp/Employee.java
@@ -2,14 +2,21 @@ package empapp;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Data
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class Employee {
 
     @Id
@@ -17,6 +24,14 @@ public class Employee {
     private Long id;
 
     private String name;
+
+    @Embedded
+    private AuditInfo auditInfo = new AuditInfo();
+
+    private Validity validity;
+
+    @Version
+    private int version;
 
     @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL)
     private List<Address> addresses = new ArrayList<>();
--- jpa-audit/src/main/java/empapp/EmployeesApplication.java
+++ jpa-audit/src/main/java/empapp/EmployeesApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EmployeesApplication
 {
 
