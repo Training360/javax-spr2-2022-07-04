c625fe91d5b54f8f02c998fc3699c8a3da155845
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-05 21:59:21 +0200

    Init

---
+++ cache-redis/src/main/java/empapp/EmployeeService.java
@@ -0,0 +1,73 @@
+package empapp;
+
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @CacheEvict(value = "employees", allEntries = true)
+    @CachePut(value = "employee", key = "#result.id")
+    public EmployeeDto createEmployee(CreateEmployeeCommand command) {
+        Employee employee = new Employee(command.getName());
+        ModelMapper modelMapper = new ModelMapper();
+        if (command.getAddresses() != null) {
+            employee.addAddresses(command.getAddresses().stream().map(a -> modelMapper.map(a, Address.class)).collect(Collectors.toList()));
+        }
+        employeeRepository.save(employee);
+        return modelMapper.map(employee, EmployeeDto.class);
+    }
+
+    @Cacheable("employees")
+    public List<EmployeeDto> listEmployees() {
+        ModelMapper modelMapper = new ModelMapper();
+        return employeeRepository.findAllWithAddresses().stream()
+                .map(e -> modelMapper.map(e, EmployeeDto.class))
+                .collect(Collectors.toList());
+    }
+
+    @Cacheable("employee")
+    public EmployeeDto findEmployeeById(long id) {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(employeeRepository.findByIdWithAddresses(id)
+                        .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id)),
+                EmployeeDto.class);
+    }
+
+    @Transactional
+    @Caching(evict = {
+            @CacheEvict(value = "employees", allEntries = true)
+            //,@CacheEvict(value = "employee", key = "#id")
+    },
+        put = {
+            @CachePut(value = "employee", key = "#id")
+        }
+    )
+    public EmployeeDto updateEmployee(long id, UpdateEmployeeCommand command) {
+        Employee employeeToModify = employeeRepository.getById(id);
+        employeeToModify.setName(command.getName());
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(employeeToModify, EmployeeDto.class);
+    }
+
+    @Caching(evict = {
+        @CacheEvict(value = "employees", allEntries = true),
+        @CacheEvict(value = "employee", key = "#id")})
+    public void deleteEmployee(long id) {
+        Employee employee = employeeRepository.findByIdWithAddresses(id)
+                .orElseThrow(() -> new NotFoundException("Employee not found with id: " + id));
+        employeeRepository.delete(employee);
+    }
+}
