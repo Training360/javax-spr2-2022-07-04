8071f24c9f6d5b99f78d4144ede4c05b9d11f3de
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-06 11:20:11 +0200

    WebSocket küldés

---
+++ websocket-client/src/main/java/training/websocketclient/MessageCommand.java
@@ -0,0 +1,13 @@
+package training.websocketclient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MessageCommand {
+
+    private String content;
+}
--- websocket-client/src/main/java/training/websocketclient/WebsocketClientApplication.java
+++ websocket-client/src/main/java/training/websocketclient/WebsocketClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandler;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
@@ -14,6 +15,8 @@ import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class WebsocketClientApplication implements CommandLineRunner {
@@ -29,7 +32,12 @@ public class WebsocketClientApplication implements CommandLineRunner {
 		WebSocketStompClient stompClient = new WebSocketStompClient(client);
 		stompClient.setMessageConverter(new MappingJackson2MessageConverter());
 		StompSessionHandler sessionHandler = new MessageStompSessionHandler();
-		stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler);
-		new Scanner(System.in).nextLine();
+		Future<StompSession> future = stompClient.connect("ws://localhost:8080/websocket-endpoint", sessionHandler);
+		StompSession session = future.get(10, TimeUnit.SECONDS);
+		String line = null;
+		do {
+			line = new Scanner(System.in).nextLine();
+			session.send("/app/messages", new MessageCommand(line));
+		} while (!line.trim().equals(""));
 	}
 }
