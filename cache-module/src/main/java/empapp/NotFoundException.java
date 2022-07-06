b9ff395ae6868cdf3a3aa7d6ef4e57b00b5c0156
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-06 10:03:20 +0200

    Alkalmazás Java Platform Modul Systemmel

--- cache-module/src/main/java/empapp/NotFoundException.java
+++ cache-module/src/main/java/empapp/NotFoundException.java
@@ -1,13 +1,11 @@
 package empapp;
 
-import org.zalando.problem.AbstractThrowableProblem;
-import org.zalando.problem.Status;
 
 import java.net.URI;
 
-public class NotFoundException extends AbstractThrowableProblem {
+public class NotFoundException extends RuntimeException {
 
     public NotFoundException(String message) {
-        super(URI.create("not-found"), "Not found", Status.NOT_FOUND, message);
+        super(message);
     }
 }
