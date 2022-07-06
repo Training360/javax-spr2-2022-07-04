2c86447de370f0fc14d423bb0f3ad22d00401cb7
Istvan Viczian <viczian.istvan@gmail.com>
2022-07-06 11:00:22 +0200

    Init

---
+++ websocket-client/src/test/java/training/websocketclient/WebsocketClientApplicationTests.java
@@ -0,0 +1,13 @@
+package training.websocketclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
